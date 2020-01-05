283afdcc0698d075d7c054ab9cc93bdb3f89c744
Jacob Lara <l1a9r9a7@gmail.com>
2020-01-05 14:56:17 -0700

    update

---
+++ models/burger.js
@@ -0,0 +1,8 @@
+module.exports = function (Sequelize, Datatypes) {
+    const Burger = Sequelize.define("burger", {
+        name: Datatypes.STRING,
+        eaten: Datatypes.BOOLEAN
+    })
+
+    return Burger
+}
